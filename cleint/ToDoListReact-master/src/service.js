1b62214f65705438df16a9e8167275da7104e884
r0533130357-dev <0533130357@gmail.com>
2025-11-27 02:35:41 +0200

    commit6

--- cleint/ToDoListReact-master/src/service.js
+++ cleint/ToDoListReact-master/src/service.js
@@ -1,35 +1,8 @@
-// import axios from 'axios';
-// const apiUrl = process.env.REACT_APP_API_URL;
-// export default {
-//   getTasks: async () => {
-//     const result = await axios.get(`${apiUrl}/tasks`);
-//     return result.data;
-//   },
-
-//   addTask: async (name) => {
-//     const result = await axios.post(`${apiUrl}/tasks`, { name, isComplete: false });
-//     return result.data;
-//   },
-
-  // setCompleted: async (id, isComplete) => {
-  //   const result = await axios.put(`${apiUrl}/tasks/${id}`, { isComplete });
-  //   return result.data;
-  // },
-
-//   deleteTask: async (id) => {
-//     const result = await axios.delete(`${apiUrl}/tasks/${id}`);
-//     return result.data;
-//   }
-// };
-// setCompleted: async (id, task) => {
-//   const result = await axios.put(`${apiUrl}/tasks/${id}`, task);
-//   return result.data;
-// };
-
-
-import axios from 'axios';
-
+ import axios from 'axios';
 const apiUrl = process.env.REACT_APP_API_URL;
+
+
+
 
 export default {
   getTasks: async () => {
@@ -53,23 +26,31 @@ export default {
     }
   },
 
-  setCompleted: async (task) => {
-    try {
-      if (!task.id) {
-        throw new Error("Task ID is missing!");
-      }
+  // setCompleted: async (task) => {
+  //   try {
+  //     if (!task.id) {
+  //       throw new Error("Task ID is missing!");
+  //     }
 
-      const result = await axios.put(`${apiUrl}/tasks/${task.id}`, task, {
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-      return result.data;
-    } catch (error) {
-      console.error("Error updating task:", error);
-      throw error;
-    }
+  //     const result = await axios.put(`${apiUrl}/tasks/${task.id}`, task, {
+  //       headers: {
+  //         'Content-Type': 'application/json'
+  //       }
+  //     });
+  //     return result.data;
+  //   } catch (error) {
+  //     console.error("Error updating task:", error);
+  //     throw error;
+  //   }
+  // },
+
+  
+  setCompleted: async (id, isComplete) => {
+    const result = await axios.put(`${apiUrl}/tasks/${id}`, { isComplete });
+    return result.data;
   },
+
+
 
   deleteTask: async (id) => {
     try {
